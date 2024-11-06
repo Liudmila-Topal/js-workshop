f2b8dee19c0468549b14961ec1c2f7f82f3ba878
caheletavi <valentin.enache91@yahoo.ro>
2024-11-06 15:18:53 +0200

    Remove HW Solution

--- src/test/js.workshop/homework/c.conditions&loops/conditionsExercises.js
+++ src/test/js.workshop/homework/c.conditions&loops/conditionsExercises.js
@@ -4,21 +4,13 @@ Exercise 1: Simple if Statement
 */
 let num = 15;
 // Write your conditional here
-if (num > 10) {
-    console.log('The number is greater than 10');
-}
 
 /*
 Exercise 2. if-else Statement
 Write a JavaScript program that checks if a number is odd or even. If the number is even, log 'The number is even', otherwise log 'The number is odd'.
 */
-// let num = 7;
+let num = 7;
 // Write your conditional here
-if (num % 2 === 0) {
-    console.log('The number is even');
-} else {
-    console.log('The number is odd');
-}
 
 /*
 Exercise 3. Ternary Operator
@@ -26,8 +18,6 @@ Use the ternary operator to check if a person is old enough to drink (18+). Log 
 */
 let age = 20;
 // Write your conditional here
-const result = age >= 18 ? 'Allowed to drink' : 'Not allowed to drink'
-console.log(result);
 
 /*
 Exercise 4. else if Ladder
@@ -41,17 +31,6 @@ Log the appropriate message.
 */
 let score = 75;
 // Write your conditional here
-if (score >= 90) {
-    console.log('Grade A');
-} else if (score >= 80) {
-    console.log('Grade B');
-} else if (score >= 70) {
-    console.log('Grade C');
-} else if (score >= 60) {
-    console.log('Grade D');
-} else {
-    console.log('Grade F');
-}
 
 
 /*
@@ -65,17 +44,6 @@ Log the appropriate message for each case.
 let memberAge = 45;
 let isMember = true;
 // Write your nested if condition here
-if (isMember) {
-    if (memberAge < 18) {
-        console.log('You get youth discount of 5%')
-    } else if (memberAge >= 65) {
-        console.log('You get senior discount of 15%')
-    } else {
-        console.log('You get adult discount of 10%')
-    }
-} else {
-    console.log('You are not eligible for any discount')
-}
 
 /*
 Exercise 6. Checking Multiple Conditions (&& and ||)
@@ -87,11 +55,6 @@ let personAge = 17;
 let isCitizen = true;
 let hasSpecialPermission = true;
 // Write your conditional here
-if ((personAge >= 18 && isCitizen) || (personAge >= 16 && hasSpecialPermission)) {
-    console.log('Person is eligible to vote');
-} else {
-    console.log('Person is not eligible to vote');
-}
 
 /*
 Exercise 7. Switch Statement: Traffic Light System
@@ -102,19 +65,6 @@ Write a program that simulates a traffic light system. Based on the input color 
 */
 let trafficLight = 'yellow';
 // Write your switch statement here
-switch (trafficLight.toLowerCase()) {
-    case 'red':
-        console.log('Stop');
-        break;
-    case 'yellow':
-        console.log('Get ready');
-        break;
-    case 'green':
-        console.log('Go');
-        break;
-    default:
-        console.log('Invalid input');
-}
 
 /*
 Exercise 8. Checking for Truthy/Falsy Values
@@ -122,24 +72,10 @@ Write a program that checks if a variable input has a value. If it is falsy (che
 */
 let input = '';
 // Write your conditional here
-if (input) {
-    console.log('The input value is: ' + input);
-} else {
-    console.log('No input provided');
-}
 
 /*
 Exercise 9. Conditional Based on Type
 Write a program that checks the type of a variable and logs a different message depending on whether it's a string, number, boolean or something else.
 */
-let value = null;
+let value = 'Hello';
 // Write your conditional here
-if (typeof value === 'string') {
-    console.log('The variable is a string');
-} else if (typeof value === 'number') {
-    console.log('The variable is a number');
-} else if (typeof value === 'boolean') {
-    console.log('The variable is a boolean');
-} else {
-    console.log('The variable is something else');
-}
--- src/test/js.workshop/homework/c.conditions&loops/loopsExercises.js
+++ src/test/js.workshop/homework/c.conditions&loops/loopsExercises.js
@@ -6,69 +6,26 @@ Log the result.
 */
 
 // //For loop
-let sum = 0;
-for (let i = 0; i <= 50; i++) {
-    sum += i;
-}
-console.log(sum);
 
 //While loop
-let sum = 0;
-let i = 0;
-while (i <= 50) {
-    sum += i;
-    i++;
-}
-console.log(sum);
 
 //Do...while loop
-let sum = 0;
-let i = 0;
-do {
-    sum += i;
-    i++;
-} while (i <= 50);
-console.log(sum);
 
 /*
 Exercise 2. Find Even Numbers Using continue
 Write a program that uses a for loop to print all the even numbers between 1 and 20.
 Hint: Use continue to skip the odd numbers.
 */
-for (let i = 0; i <= 20; i++) {
-    if (i % 2 !== 0) {
-        continue;
-    }
-    console.log(i);
-}
 
 /*
 Exercise 3. Breaking Out of a Loop
 Write a program that uses a while loop to print numbers from 1 to 100, but stops when it reaches a number divisible by 25 and is even.
 */
-let num = 1;
-while (num <= 100) {
-    if (num % 25 === 0 && num % 2 === 0) {
-        console.log(`The loops breaks at ${num} iteration`);
-        break;
-    }
-    console.log(num);
-    num++;
-}
 
 /*
 Exercice 4. Find the First Odd Number Using break
 Write a program that uses a for..of loop to find the first odd number in an array [2, 4, 6, 7, 8, 10] and breaks the loop once it finds the odd number.
 */
-let myArray = [2, 4, 6, 7, 8, 10];
-for (let nr of myArray) {
-    if (nr % 2 !== 0) {
-        console.log(`first odd number in array is ${nr}`);
-        break;
-    } else {
-        console.log(`${nr} is even number`)
-    }
-}
 
 /*
 BONUS:
@@ -89,21 +46,3 @@ model: Corolla
 year: 2021 - This car is new.
 color: blue - This is my favorite color!
  */
-let car = {
-    make: 'Toyota',
-    model: 'Corolla',
-    year: 2021,
-    color: 'blue'
-};
-let currentYear = 2024;
-
-for (let property in car) {
-    if (property === 'year') {
-        let carAge = (currentYear - car[property]) <= 3 ? 'new' : 'not new';
-        console.log(`${property}: ${car[property]} - This car is ${carAge}.`);
-    } else if (property === 'color' && car[property] === 'blue') {
-        console.log(`${property}: ${car[property]} - This is my favorite color!`);
-    } else {
-        console.log(`${property}: ${car[property]}`);
-    }
-}
